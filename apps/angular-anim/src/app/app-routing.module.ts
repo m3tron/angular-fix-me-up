ebf809f70e97f0bcbcc100169dbb2410a33d0b60
Sunny <sunny.heyar@gmail.com>
2022-09-22 11:12:36 -0400

    wired up transfers component in app-routing

--- apps/angular-anim/src/app/app-routing.module.ts
+++ apps/angular-anim/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { AccountDetailsComponent } from './account-details/account-details.component';
+import { TransfersComponent } from './transfers/transfers.component';
 
 // DONE: 2. We've setup these routes and have them on the page but they aren't working
 const routes: Routes = [
   { path: '', component: AccountSummaryComponent },
   { path: 'about', component: AboutComponent },
   { path: 'account/:id', component: AccountDetailsComponent },
+  { path: 'transfers', component: TransfersComponent },
 ];
 
 @NgModule({
--- apps/angular-anim/src/app/transfers/transfers.component.ts
+++ apps/angular-anim/src/app/transfers/transfers.component.ts
@@ -5,8 +5,4 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './transfers.component.html',
   styleUrls: ['./transfers.component.css'],
 })
-export class TransfersComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
-}
+export class TransfersComponent {}
